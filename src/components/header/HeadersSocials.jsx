f0b2a785edaabfc6ce423c1f7fee6ccd9e3d1a6c
M H Hasib <hasib.mahmudul@gmail.com>
2023-06-26 17:20:10 +0600

    feat: wip header

---
+++ src/components/header/HeadersSocials.jsx
@@ -0,0 +1,14 @@
+import { BsLinkedin } from "react-icons/bs";
+
+const HeadersSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="__blank"></a>
+      <a href="https://github.com" target="__blank"></a>
+      <a href="https://youtube.com" target="__blank"></a>
+      <a href="https://twitter.com" target="__blank"></a>
+    </div>
+  );
+};
+
+export default HeadersSocials;
